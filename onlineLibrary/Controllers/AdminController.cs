873744685a3582118b934d301c1dddbd605fd0b2
Egor Petko <egrpetko@gmail.com>
2024-01-26 14:45:12 +0300

    added test AdminController

---
+++ onlineLibrary/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("[Action]")]
+        public ActionResult<string> Send()
+        {
+            return "Admin!";
+        }
+    }
+}
--- onlineLibrary/Program.cs
+++ onlineLibrary/Program.cs
@@ -1,6 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using onlineLibrary.Context;
+using onlineLibrary.UserEntities;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.Map("/add", async () =>
+{
+    using var db = new ApplicationContext();
+
+        // создаем два объекта User
+    UserEntity tom = new UserEntity { Name = "Tom", Password = "p33", Post = UserPost.User};
+    UserEntity alice = new UserEntity { Name = "Alice", Password = "p26", Post = UserPost.User};
+
+    // добавляем их в бд
+    db.Users.Add(tom);
+    db.Users.Add(alice);
+    await db.SaveChangesAsync();
+    Console.WriteLine("Объекты успешно сохранены");
+});
+app.Map("/show", () =>
+{
+    using(ApplicationContext db = new ApplicationContext())
+    {
+        var users = db.Users.ToList();
+        Console.WriteLine("Список объектов:");
+        foreach (UserEntity u in users)
+        {
+            Console.WriteLine($"{u.Id}.{u.Name} - {u.Password}");
+        }
+    }
+});
+app.MapControllers();
 
 app.Run();
+
